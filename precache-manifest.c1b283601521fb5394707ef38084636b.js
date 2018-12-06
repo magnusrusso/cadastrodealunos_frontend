111622d3f13a3c01c3ce99a32302b96dcdcb9fc4
Magnus Russo <magnusrusso@yahoo.com.br>
2018-12-06 11:24:44 -0200

    Updates

---
+++ precache-manifest.c1b283601521fb5394707ef38084636b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d65bf568b06dd0bd29",
+    "url": "/cadastrodealunos_frontend/static/css/main.edbf2dd8.chunk.css"
+  },
+  {
+    "revision": "b5d65bf568b06dd0bd29",
+    "url": "/cadastrodealunos_frontend/static/js/main.b5d65bf5.chunk.js"
+  },
+  {
+    "revision": "ad96b63e724ac0490a73",
+    "url": "/cadastrodealunos_frontend/static/js/1.ad96b63e.chunk.js"
+  },
+  {
+    "revision": "9419b2556fe6adb35947",
+    "url": "/cadastrodealunos_frontend/static/js/runtime~main.9419b255.js"
+  },
+  {
+    "revision": "45ed1b3d7ff836a665914b4125e4e899",
+    "url": "/cadastrodealunos_frontend/index.html"
+  }
+];
